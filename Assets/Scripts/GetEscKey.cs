54d1306a0854afd5f82264e622dd386b76d3a2d1
Logan Eller <34076740+LoganTCP@users.noreply.github.com>
2024-03-17 17:58:58 -0400

    Solved the pause menu issue
    Instead of disabling the FirstPersonController script, just disable the entire thing, switching the view back to the MainCamera which produces a similar effect.

--- Assets/Scripts/GetEscKey.cs
+++ Assets/Scripts/GetEscKey.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class GetEscKey : MonoBehaviour
 {
     public GameObject PauseMenu;
+    public GameObject FPSController;
+    public GameObject MainCamera;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("escape"))
         {
             PauseMenu.SetActive(true);
+            MainCamera.SetActive(true);
+            FPSController.SetActive(false);
         }
     }
 }
